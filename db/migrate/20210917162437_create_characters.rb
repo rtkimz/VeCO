e1ddf0b61cffe9a109d7edf234c11a707ea6d6f2
Robin <robinkimzey@gmail.com>
2021-09-17 09:26:24 -0700

    add Character model

---
+++ db/migrate/20210917162437_create_characters.rb
@@ -0,0 +1,10 @@
+class CreateCharacters < ActiveRecord::Migration[6.1]
+  def change
+    create_table :characters do |t|
+      t.string :name, default: "various"
+      t.string :publisher
+
+      t.timestamps
+    end
+  end
+end
