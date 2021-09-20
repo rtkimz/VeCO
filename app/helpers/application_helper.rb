489bad5da7925d1532fb8d22df1cdaec3f76a9bc
Robin <robinkimzey@gmail.com>
2021-09-20 14:57:05 -0700

    add a human_readable_size method to the helper file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def human_readable_size(size)
+    units = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'Pib', 'EiB']
+  
+    return '0.0 B' if size == 0
+    exp = (Math.log(size) / Math.log(1024)).to_i
+    exp += 1 if (size.to_f / 1024 ** exp >= 1024 - 0.05)
+    exp = 6 if exp > 6 
+  
+    '%.1f %s' % [size.to_f / 1024 ** exp, units[exp]]
+  end
+
 end
