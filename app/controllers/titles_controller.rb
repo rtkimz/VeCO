ea8b3f7f8b3eefffb8c55c486e965fb14211a6fa
Robin <robinkimzey@gmail.com>
2021-09-26 16:50:39 -0700

    add various and functionality to some of the methods in the titles_controller

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -1,15 +1,27 @@
 class TitlesController < ApplicationController
 
   def index
+    @comics = Comic.joins(title: :publisher, title: :character)
+    @titles = Title.joins(:publisher, :character)
   end
 
   def create 
   end
 
   def edit
+    @title = Title.find(params[:id])
+    @publishers = Publisher.all
+    @characters = Character.all
+  end
+
+  def update
+    title = Title.find(params[:id])
+    title.update(publisher_id: params[:publisher_id], character_id: params[:character_id])
+    redirect "/title/#{params[:id]}/edit"
   end
 
   def delete 
+    @title = Title.find(params[:id])
   end
-  
+
 end
