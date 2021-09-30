2cce7fdcd59860ea1b4e5d7ee119b4b2864941e5
Robin <robinkimzey@gmail.com>
2021-09-30 14:41:07 -0700

    remove the scan_comics_controller and scrape.rb files because the functionality found in those files has been implemented in the db:seeds task

--- app/controllers/scan_comics_controller.rb
+++
@@ -1,2 +0,0 @@
-class ScanComicsController < ApplicationController
-end
--- test/controllers/scan_comics_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ScanComicsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
