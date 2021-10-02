8aaf9ed569a1a3b91eff97c51032a81dd21d75b8
Robin <robinkimzey@gmail.com>
2021-10-01 18:16:48 -0700

    add a controller and views for comic book publishers

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -4,4 +4,14 @@ class PublishersController < ApplicationController
     @publishers = Publisher.all
   end
 
+  def show
+    @comics = Comic.joins(:title).where(title: {publisher_id: params[:id]}).order(:name)
+    @char_set = {}
+    @comics.each do |comic|
+      @char_set.store(comic.title.character.id, comic.title.character.name)
+    end
+    temp_array = @char_set.sort_by(&:last)
+    @char_set = temp_array.to_h
+  end
+
 end
