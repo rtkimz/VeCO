52cd6624fb59cff6ad7ba7ac5c2b685e4789669c
Robin <robinkimzey@gmail.com>
2021-10-01 18:23:20 -0700

    add routes for all functionality currently available in the application

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "publishers#index"
+  
+  get "publishers" => "publishers#index"
+  get "publisher/:id" => "publishers#show", as: "publisher"
+
+  get "character/:id" => "characters#show", as: "character"
+
+  get "titles" => "titles#index"
+  get "title/:id" => "titles#show", as: "title"
+
 end
